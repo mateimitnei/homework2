6a1770549d1e07d76f2cafc8c5402c39ac4a7ace
Matei M <matei.mitnei@m-tag.io>
2024-09-26 21:29:22 +0300

    Reviewed

--- homeWorkTemplate/js/main.js
+++ homeWorkTemplate/js/main.js
@@ -1,39 +1,31 @@
 const btn = document.getElementById("play");
+const newArr = [...data];
 
 function transform() {
     // 1. Remove 6th element
-    data.splice(5, 1);
-    // 2. Remove id field
-    const idlessArr = [];
-    data.forEach(function(item, index=0){
-        idlessArr.push(item);
-        delete idlessArr[index].id;
-    })
+    newArr.splice(5, 1);
     // 3. Map
-    const mappedArr = idlessArr.map(function(item){
-        // 4. Create a new array with objects
-        let newItem = {};
-        // 5. Modify name
-        newItem.name = item.name[0].toUpperCase() + item.name.slice(1).toLowerCase();
-        // 6. Modify link
-        newItem.url = "http://" + item.url;
-        // 7. Modify description
-        newItem.description = item.description.slice(0, 15) + "...";
-        // 8. Create date
+    const filteredArr = newArr.map(function(item){
         const tmpDate = new Date(item.date);
-        newItem.date = tmpDate.getFullYear() + "/" +
-            tmpDate.getMonth() + "/" +
-            tmpDate.getDate() + " " +
-            tmpDate.getHours() + ":" +
-            tmpDate.getMinutes();
-        // 10. Modify params
-        newItem.params = `${item.params.status}=>${item.params.progress}`;
-        // 11. Create isVisible
-        newItem.isVisible = item.params.status;
-        return newItem;
-    })
-    // 12. Filter
-    const filteredArr = mappedArr.filter(function(item){
+        return {
+            // 5. Modify name
+            name : item.name[0].toUpperCase() + item.name.slice(1).toLowerCase(),
+            // 6. Modify link
+            url : "http://" + item.url,
+            // 7. Modify description
+            description : item.description.slice(0, 15) + "...",
+            // 8. Create date
+            date : tmpDate.getFullYear() + "/" +
+                tmpDate.getMonth() + "/" +
+                tmpDate.getDate() + " " +
+                tmpDate.getHours() + ":" +
+                tmpDate.getMinutes(),
+            // 10. Modify params
+            params : `${item.params.status}=>${item.params.progress}`,
+            // 11. Create isVisible
+            isVisible : item.params.status
+        }
+    }).filter(function(item){
         return item.isVisible === true;
     });
 
